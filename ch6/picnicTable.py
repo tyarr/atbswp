8f8e11309e864f80953f625b8ae918b90b5bb804
tyarr <tyler.yarborough@gmail.com>
2017-02-23 20:13:09 -0700

    Change test

--- ch6/picnicTable.py
+++ ch6/picnicTable.py
@@ -5,4 +5,4 @@ def printPicnic(itemsDict, leftWidth, rightWidth):
 
 picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
 printPicnic(picnicItems, 12, 5)
-printPicnic(picnicItems, 20, 6)
+#printPicnic(picnicItems, 20, 6)
