2a4c841c6cfa603db7754e5c3fee49e1a7f62806
tyarr <tyler.yarborough@gmail.com>
2017-05-11 21:00:59 -0700

    new scritps 2017-05-11

---
+++ ch7/madLibGame.py
@@ -0,0 +1,16 @@
+# Input data
+print('Enter an adjective: ')
+a1 = input()
+print('Enter a noun: ')
+n1 = input()
+print('Enter a verb: ')
+v1 = input()
+print('Enter a noun:')
+n2 = input()
+
+# Print to console
+print('The ' + a1 + ' panda walked to the ' + n1 + ' and then ' + v1 + '. A nearby ' + n2 + ' was unaffected by these events.')
+
+# Save input
+with open('output.txt', 'w') as f:
+    print('The ' + a1 + ' panda walked to the ' + n1 + ' and then ' + v1 + '. A nearby ' + n2 + ' was unaffected by these events.', file=f)
